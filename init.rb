f8b0f943479702e87af2efaf8a42a120046f41ad
Neeraj Singh <neerajdotname@gmail.com>
2009-05-25 00:23:56 -0400

    updated requirement for rails 2.2 and higher

--- init.rb
+++ init.rb
@@ -21,7 +21,11 @@ require 'admin_data_date_validation'
 require 'admin_data_helpers'
 
 if Rails.version < "2.2.0"
-  raise 'plugin admin_data only works with Rails 2.2 and higher' 
+  raise %(
+  plugin admin_data only works with Rails 2.2 and higher
+  )
 elsif Rails.version > '2.2.0' && Rails.version < '2.3.0'
-  raise 'this plugin admin_data only works with Rails 2.3 and higher. You are using Rails 2.2 . Please read README on how to use this plugin with Rails 2.2'   
+  raise %(
+  This version of plugin admin_data only works with Rails 2.3 and higher. You are using Rails 2.2 . Please read README on how to use this plugin with Rails 2.2'   
+  )
 end
