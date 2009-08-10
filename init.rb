f9398cb7027e5238e9b1eaa1c07e07fd91da3732
Neeraj Singh <neerajdotname@gmail.com>
2009-08-10 12:20:50 -0400

    revision 0.0.3

--- init.rb
+++ init.rb
@@ -1,28 +1,35 @@
-require 'rubygems'
+# not requiring rubygems because of this http://gist.github.com/54177
+#require 'rubygems'
 
+# do not want to raise exception if will_paginate is missing because running
+# rake gems:install will never complete
+# this is the reason why this method returns a boolean value
 def load_will_paginate
-  begin
-    require 'will_paginate' 
-  rescue LoadError => e
+   begin
+      require 'will_paginate'
+      true
+   rescue LoadError => e
       $stderr.puts %(
       ***********************************************
       * gem will_paginate is missing                *
-      * Please install will_paginate by executing   * 
+      * Please install will_paginate by executing   *
       * gem sources -a http://gems.github.com       *
       * sudo gem install mislav-will_paginate       *
       ***********************************************
       )
-      raise e
-    end
+      false
+   end
 end
 
-load_will_paginate
-  
-require 'admin_data_date_validation'
-require 'admin_data_helpers'
+if load_will_paginate
 
-if Rails.version < "2.2.0"
-  raise %( plugin admin_data only works with Rails 2.2 and higher)
-elsif Rails.version > '2.2.0' && Rails.version < '2.3.0'
-  raise %( This version of plugin admin_data only works with Rails 2.3 and higher. You are using Rails 2.2 . Please read README on how to use this plugin with Rails 2.2'   )
+   require 'admin_data_date_validation'
+   require 'admin_data_helpers'
+
+   if Rails.version < "2.2.0"
+      raise %( plugin admin_data only works with Rails 2.2 and higher)
+   elsif Rails.version > '2.2.0' && Rails.version < '2.3.0'
+      raise %( This version of plugin admin_data only works with Rails 2.3 and higher. You are using Rails 2.2 . Please read README on how to use this plugin with Rails 2.2'   )
+   end
+
 end
