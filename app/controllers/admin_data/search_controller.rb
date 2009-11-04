91f90bf74eebeb553931d7c9d15b3ea944d4f0ec
SubbaRao Pasupuleti <subbarao.pasupuleti@gmail.com>
2009-11-03 22:49:32 -0500

    refractoring of advance search

--- app/controllers/admin_data/search_controller.rb
+++ app/controllers/admin_data/search_controller.rb
@@ -1,4 +1,5 @@
 class AdminData::SearchController  < AdminData::BaseController 
+   include Search
 
   unloadable
 
@@ -69,115 +70,6 @@ class AdminData::SearchController  < AdminData::BaseController
 
     condition = attribute_conditions.join(' or ')
     [condition, {:search_term => "%#{search_term.downcase}%"}]
-  end
-
-  def build_advance_search_conditions(klass, search_options)
-    return {:cond => '', :errors => []} if search_options.blank?
-
-    attribute_conditions = []
-    errors = []
-
-    search_options.each do |key,value|
-      col1 = value[:col1]
-      
-      # col1 value is directly used in the sql statement. So it is important to sanitize it
-      col1 = col1.gsub(/\W/,'')
-
-      col2 = value[:col2]
-      col3 = value[:col3]
-      col3 = col3.downcase.strip unless col3.blank?
-
-      table_name = klass.table_name
-
-      like_operator = ActiveRecord::Base.connection.adapter_name == 'PostgreSQL' ? 'ILIKE' : 'LIKE'
-
-      case col2
-      when 'contains':
-        attribute_conditions << ["#{table_name}.#{col1} #{like_operator} ?","%#{col3}%"] unless col3.blank?
-
-      when 'is_exactly':
-        attribute_conditions << ["#{table_name}.#{col1} = ?",col3] unless col3.blank?
-
-      when 'does_not_contain':
-        unless col3.blank?
-          q = "#{table_name}.#{col1} is null OR "
-          q << "#{table_name}.#{col1} NOT #{like_operator} ?"
-          attribute_conditions << [q, "%#{col3}%"] 
-        end
-
-      when 'is_false':
-        attribute_conditions << ["#{table_name}.#{col1} = ?",false]
-
-      when 'is_true':
-        attribute_conditions << ["#{table_name}.#{col1} = ?",true]
-
-      when 'is_null':
-        attribute_conditions << "#{table_name}.#{col1} IS NULL"
-
-      when 'is_not_null':
-        attribute_conditions << "#{table_name}.#{col1} IS NOT NULL"
-
-      when 'is_on':
-        time_obj = AdminDataDateValidation.validate(col3)
-        if time_obj
-          attribute_conditions << ["#{table_name}.#{col1} >= ?",time_obj.beginning_of_day]
-          attribute_conditions << ["#{table_name}.#{col1} < ?",time_obj.end_of_day]
-        else
-           errors << "#{col3} is not a valid date"
-        end
-
-      when 'is_on_or_before_date':
-        time_obj = AdminDataDateValidation.validate(col3)
-        if time_obj 
-          attribute_conditions << ["#{table_name}.#{col1} <= ?",time_obj.end_of_day]
-        else
-           errors << "#{col3} is not a valid date"
-        end
-
-      when 'is_on_or_after_date':
-        time_obj = AdminDataDateValidation.validate(col3)
-        if time_obj
-          attribute_conditions << ["#{table_name}.#{col1} >= ?",time_obj.beginning_of_day]
-        else
-           errors << "#{col3} is not a valid date"
-        end
-
-      when 'is_equal_to':
-         unless col3.blank?
-            if is_integer?(col3) 
-               attribute_conditions << ["#{table_name}.#{col1} = ?",col3.to_i] 
-            else
-               errors << "#{col3} is not a valid integer"
-            end
-         end
-
-      when 'greater_than':
-         unless col3.blank?
-            if is_integer?(col3)
-               attribute_conditions << ["#{table_name}.#{col1} > ?",col3.to_i] 
-            else
-               errors << "#{col3} is not a valid integer"
-            end
-         end
-
-      when 'less_than':
-         unless col3.blank?
-            if is_integer?(col3)
-               attribute_conditions << ["#{table_name}.#{col1} < ?",col3.to_i] 
-            else
-               errors << "#{col3} is not a valid integer"
-            end
-         end
-
-      else
-         # it means user did not select anything in col2. Ignore it.
-      end
-
-    end # end of search_options loop
-
-    # queries are joined by AND
-    cond = klass.send(:merge_conditions, *attribute_conditions)
-    {:cond => cond, :errors => errors}
   end
 
   def table_name_and_attribute_name(klass,column)
