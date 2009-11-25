8086854fa262891854e9486282dc9e29d0441175
Neeraj Singh <neerajdotname@gmail.com>
2009-11-25 13:52:53 -0500

    upgrading test environment to rails 2.3.4

--- test/rails_root/config/environment.rb
+++ test/rails_root/config/environment.rb
@@ -1,6 +1,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 old_verbose, $VERBOSE = $VERBOSE, nil
-RAILS_GEM_VERSION = '= 2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '= 2.3.4' unless defined? RAILS_GEM_VERSION
 $VERBOSE = old_verbose
 
 require File.join(File.dirname(__FILE__), 'boot')
