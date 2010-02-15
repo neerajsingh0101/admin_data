4db513d3adb4522574c3301f688afa8c4abe168d
Neeraj Singh <neerajdotname@gmail.com>
2010-02-15 15:56:37 -0500

    javascript testing

--- app/controllers/admin_data/migration_controller.rb
+++ app/controllers/admin_data/migration_controller.rb
@@ -2,11 +2,19 @@ class AdminData::MigrationController < AdminData::BaseController
 
   unloadable
 
+  layout nil, :only => [:jstest]
+
   def index
     @page_title = 'migration information'
     m = 'select * from schema_migrations'
     @data = ActiveRecord::Base.connection.select_all(m)
     respond_to {|format| format.html}
   end
+
+  def jstest
+    @page_title = 'jstest'
+    respond_to {|format| format.html}
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ ActionController::Routing::Routes.draw do |map|
 
     admin_data.with_options :controller => 'migration' do |m|
       m.migration_information       '/migration',                       :action => 'index'
+      m.jstest                      '/jstest',                          :action => 'jstest'
     end
 
     admin_data.with_options :controller => 'feed' do |m|
@@ -30,6 +31,7 @@ ActionController::Routing::Routes.draw do |map|
       m.search                      '/quick_search/:klass',             :action => 'quick_search'
       m.advance_search              '/advance_search/:klass',           :action => 'advance_search'
     end
+
 
     admin_data.resources  :on_k,
                           :as => ':klass',
