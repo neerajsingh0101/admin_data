abbf3a626196053fec1afc9fb08e6e6e60f5d96b
Neeraj Singh <neerajdotname@gmail.com>
2010-08-11 11:36:07 -0400

    if will_paginate gem is missing then no need to load rest of stuff

--- init.rb
+++ init.rb
@@ -1,48 +1,39 @@
-AdminData::Version = AdminData::VERSION = '0.0.5'
-
-
-# not requiring rubygems because of this http://gist.github.com/54177
-#require 'rubygems'
-
-# do not want to raise exception if will_paginate is missing because running
+# do not raise exception if will_paginate is missing because running
 # rake gems:install will never complete
-# this is the reason why this method returns a boolean value
 def load_will_paginate
-   begin
-      require 'will_paginate'
-      true
-   rescue LoadError => e
-      $stderr.puts %(
-      ***********************************************
-      * gem will_paginate is missing                *
-      * plugin admin_data depends on will_paginate  * 
-      * Please install will_paginate by executing   *
-      * sudo gem install will_paginate              *
-      ***********************************************
-      )
-      false
-   end
+  begin
+    require 'will_paginate'
+    true
+  rescue LoadError => e
+    $stderr.puts %(
+    ***********************************************
+    * gem will_paginate is missing                *
+    * plugin admin_data depends on will_paginate  *
+    * Please install will_paginate by executing   *
+    * sudo gem install will_paginate              *
+    ***********************************************
+    )
+    false
+  end
 end
 
 if load_will_paginate
 
-   require 'admin_data_date_validation'
+  require 'admin_data_date_validation'
 
-   if Rails.version < "2.2.0"
-      raise %( plugin admin_data only works with Rails 2.2 and higher)
-   elsif Rails.version > '2.2.0' && Rails.version < '2.3.0'
-      raise %( This version of plugin admin_data only works with Rails 2.3 and higher. You are using Rails 2.2 . Please read README on how to use this plugin with Rails 2.2'   )
-   end
+  if Rails.version < "2.2.0"
+    raise %( plugin admin_data only works with Rails 2.2 and higher)
+  elsif Rails.version > '2.2.0' && Rails.version < '2.3.0'
+    raise %( This version of plugin admin_data only works with Rails 2.3 and higher. ) << 
+          %( You are using Rails 2.2 . Please read README on how to use this plugin with Rails 2.2')
+  end
 
+  ActionView::Base.send :include, AdminData::Helpers
+
+  require File.join(RAILS_ROOT, 'vendor', 'plugins', 'admin_data', 'lib', 'admin_data', 'compatibility.rb')
+  require File.join(RAILS_ROOT, 'vendor', 'plugins', 'admin_data', 'lib', 'admin_data', 'settings.rb')
+
+  AdminData::Config.initialize_defaults
+
+  require File.join(RAILS_ROOT, 'vendor', 'plugins', 'admin_data', 'lib', 'admin_data','util.rb')
 end
-
-ActionView::Base.send :include, AdminData::Helpers
-
-require File.join(RAILS_ROOT,'vendor', 'plugins', 'admin_data', 'lib', 'admin_data','compatibility.rb')
-require File.join(RAILS_ROOT,'vendor', 'plugins', 'admin_data', 'lib', 'admin_data','settings.rb')
-AdminData::Config.initialize_defaults
-
-require File.join(RAILS_ROOT,'vendor', 'plugins', 'admin_data', 'lib', 'admin_data','util.rb')
-
-
-$debug_admin_data = false
