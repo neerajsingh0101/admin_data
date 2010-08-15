932b92d8b7b8fc4eda1918fc17a666f8560f8ec5
Neeraj Singh <neerajdotname@gmail.com>
2010-08-15 16:34:50 -0400

    updating init.rb to have relevant message in case will_paginate is missing

--- init.rb
+++ init.rb
@@ -1,17 +1,18 @@
-# do not raise exception if will_paginate is missing because running
-# rake gems:install will never complete
+# do not raise exception if will_paginate is missing because running. rake gems:install will never complete
 def load_will_paginate
   begin
     require 'will_paginate'
     true
   rescue LoadError => e
     $stderr.puts %(
-    ***********************************************
-    * gem will_paginate is missing                *
-    * plugin admin_data depends on will_paginate  *
-    * Please install will_paginate by executing   *
-    * sudo gem install will_paginate              *
-    ***********************************************
+    *************************************************************************************************
+    *                                                                                               *
+    * gem will_paginate is missing                                                                  *
+    * plugin admin_data depends on will_paginate                                                    *
+    * Please install will_paginate by adding following line to Gemfile                              *
+    * gem 'will_paginate', :git => 'git://github.com/mislav/will_paginate.git', :branch => 'rails3' *
+    *                                                                                               *
+    *************************************************************************************************
     )
     false
   end
@@ -21,12 +22,9 @@ if load_will_paginate
 
   require 'admin_data_date_validation'
 
-  if Rails.version < "2.2.0"
-    raise %( plugin admin_data only works with Rails 2.2 and higher)
-  elsif Rails.version > '2.2.0' && Rails.version < '2.3.0'
-    raise %( This version of plugin admin_data only works with Rails 2.3 and higher. ) << 
-          %( You are using Rails 2.2 . Please read README on how to use this plugin with Rails 2.2')
-  end
+  #if Rails.version < "3.0.0"
+    #raise %( This version of plugin admin_data only works with Rails 3.0.0 or higher)
+  #end
 
   ActionView::Base.send :include, AdminData::Helpers
 
