9a5e941d943a7f7f1cead9f4f573294a30853462
Neeraj Singh <neerajdotname@gmail.com>
2010-08-16 10:17:13 -0400

    wip

--- init.rb
+++ init.rb
@@ -1,3 +1,1 @@
 require 'admin_data'
-
-
--- lib/admin_data.rb
+++ lib/admin_data.rb
@@ -1,20 +1,16 @@
-# do not raise exception if will_paginate is missing because running
-# rake gems:install will never complete
-def load_will_paginate
-  begin
-    require 'will_paginate'
-    true
-  rescue LoadError => e
-    $stderr.puts %(
-    ***********************************************
-    * gem will_paginate is missing                *
-    * plugin admin_data depends on will_paginate  *
-    * Please install will_paginate by executing   *
-    * sudo gem install will_paginate              *
-    ***********************************************
-    )
-    false
-  end
+begin
+  require 'will_paginate'
+rescue LoadError => e
+  $stderr.puts %(
+  *************************************************************************************************
+  *                                                                                               *
+  * gem will_paginate is missing                                                                  *
+  * plugin admin_data depends on will_paginate                                                    *
+  * Please install will_paginate by adding following line to Gemfile                              *
+  * gem 'will_paginate', :git => 'git://github.com/mislav/will_paginate.git', :branch => 'rails3' *
+  *                                                                                               *
+  *************************************************************************************************
+  )
 end
 
 module AdminData
@@ -25,7 +21,7 @@ if Rails.version >= '3.0'
   require 'admin_data/railtie'
 else
   raise "Please see documentation at http://neerajdotname.github.com/admin_data to find out how to use this plugin with rails 2.3"
-else
+end
 
 require 'admin_data_date_validation'
 require 'admin_data/helpers'
