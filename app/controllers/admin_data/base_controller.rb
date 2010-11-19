b29c45f1c10e29190e2458ebbc17fea08b3631f8
Neeraj Singh <neerajdotname@gmail.com>
2010-11-19 02:41:08 -0500

    do not look for nested model since it breaks the routing. A comprehensive support for nested models coming soon.

--- app/controllers/admin_data/base_controller.rb
+++ app/controllers/admin_data/base_controller.rb
@@ -64,7 +64,7 @@ class AdminData::BaseController < ApplicationController
       return $admin_data_all_klasses
     else
       model_dir = File.join(Rails.root, 'app', 'models')
-      model_names = Dir.chdir(model_dir) { Dir["**/*.rb"] }
+      model_names = Dir.chdir(model_dir) { Dir["*.rb"] }
       klasses = get_klass_names(model_names)
       $admin_data_all_klasses = remove_klasses_without_table(klasses).sort_by {|r| r.name.underscore}
     end
