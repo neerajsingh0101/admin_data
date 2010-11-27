57b6dbd3989587ab81f25c556fd7ac0c8e3a6380
Neeraj Singh <neerajdotname@gmail.com>
2010-11-27 10:32:28 -0500

    security fix

--- app/controllers/admin_data/public_controller.rb
+++ app/controllers/admin_data/public_controller.rb
@@ -4,16 +4,6 @@ class AdminData::PublicController < AdminData::BaseController
   def serve
     f = File.join(AdminData::Config.setting[:plugin_dir], 'lib', params[:file])
 
-    # this is needed so that a user is not able to get data by performing url like
-    # http://localhost:3000/admin_data/public/..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fetc%2Fpasswd
-    # Above url will try to get /etc/passwd
-    # 
-    # Second layer of security is below which ensures that request file must end with
-    # js or css
-    unless Regexp.new(Rails.root.to_s).match(File.expand_path(f))
-      render :nothing => true, :status => 404 and return
-    end
-
     unless File.exists?(f)
       render :nothing => true, :status => 404 and return
     end
@@ -24,6 +14,11 @@ class AdminData::PublicController < AdminData::BaseController
     elsif f =~ /js$/
       opts[:content_type] = "text/javascript"
     else
+      # this is needed so that a user is not able to get data by performing url like
+      # http://localhost:3000/admin_data/public/..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fetc%2Fpasswd
+      # Above url will try to get /etc/passwd
+      # 
+      # ensures that request file must end with js or css
       render :nothing => true, :status => 404 and return
     end
     render opts
