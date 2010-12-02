d13eb3004ff16e1f0706cfb1ee268775ee3056e2
Neeraj Singh <neerajdotname@gmail.com>
2010-12-02 13:32:20 -0500

    add comments

--- lib/admin_data/active_record_util.rb
+++ lib/admin_data/active_record_util.rb
@@ -1,18 +1,42 @@
 module AdminData
   class ActiveRecordUtil
 
+    # class User
+    #   has_and_belongs_to_many :clubs
+    # end
+    #
+    # User.declared_habtm_association_names(Club)
+    # #=> ['clubs']
     def self.declared_habtm_association_names(klass)
       delcared_association_names_for(klass, :has_and_belongs_to_many).map(&:name).map(&:to_s)
     end
 
+    # class User
+    #   belongs_to :club
+    # end
+    #
+    # User.declared_belongs_to_association_names(Club)
+    # #=> ['club']
     def self.declared_belongs_to_association_names(klass)
       delcared_association_names_for(klass, :belongs_to).map(&:name).map(&:to_s)
     end
 
+    # class User
+    #   has_one :car
+    # end
+    #
+    # User.declared_has_one_association_names(Car)
+    # #=> ['car']
     def self.declared_has_one_association_names(klass)
       delcared_association_names_for(klass, :has_one).map(&:name).map(&:to_s)
     end
 
+    # class User
+    #   has_many :cars
+    # end
+    #
+    # User.declared_has_many_association_names(Car)
+    # #=> ['cars']
     def self.declared_has_many_association_names(klass)
       delcared_association_names_for(klass, :has_many).map(&:name).map(&:to_s)
     end
@@ -28,7 +52,13 @@ module AdminData
     end
 
     # returns an array of classes
-    # #=> [Comment]
+    #
+    # class User
+    #   has_and_belongs_to_many :clubs
+    # end
+    #
+    # ActiveRecordUtil.habtm_klasses_for(User)
+    # #=> [Club]
     def self.habtm_klasses_for(klass)
       declared_habtm_association_names(klass).map do |assoc_name|
         klass_for_association_type_and_name(klass, :has_and_belongs_to_many, assoc_name)
