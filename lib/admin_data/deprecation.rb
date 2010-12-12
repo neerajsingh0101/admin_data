37eaaac4b60bf0168778f7832a132810da0902c4
Neeraj Singh <neerajdotname@gmail.com>
2010-12-11 22:04:40 -0500

    do block should end with end and not }

--- lib/admin_data/deprecation.rb
+++ lib/admin_data/deprecation.rb
@@ -5,6 +5,7 @@ class AdminData::Config
   def self.set=(input = {})
 
 msg=<<EOF
+
 AdminData API has changed in version 1.1 .
 
 The changes are very minor.
@@ -15,11 +16,11 @@ AdminData::Config.set = {
  :find_conditions => ....
 }
 
-Now you need to do
+Now you need to write in following style
 
 AdminData.config do |config|
  config.find_conditions = ...
-}
+end
 
 Please refer to documentation at https://github.com/neerajdotname/admin_data/wiki/Customizing-admin_data-for-a-Rails-3-application for more information.
 
