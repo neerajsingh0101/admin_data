efbd8012e327a93b547902daa1764f6d415d8401
Neeraj Singh <neerajdotname@gmail.com>
2010-12-22 05:13:17 -0500

    functionality moved to another file

--- lib/admin_data.rb
+++ lib/admin_data.rb
@@ -30,11 +30,7 @@ module AdminData
 
 end
 
-if Rails.version >= '3.0'
-  require 'admin_data/railtie'
-else
-  raise "Please see documentation at http://github.com/neerajdotname/admin_data/wiki to find out how to use this gem with rails 2.3.x"
-end
+require 'admin_data/railtie'
 
 # move date_validation to inside admin_data
 require 'admin_data_date_validation'
