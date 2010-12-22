af792ca9dfb3906154769a9b809404705b55f5d5
Neeraj Singh <neerajdotname@gmail.com>
2010-12-22 05:28:17 -0500

    cleaning up date validation

--- lib/admin_data_date_validation.rb
+++ lib/admin_data_date_validation.rb
@@ -2,28 +2,15 @@ require 'date'
 
 class AdminDataDateValidation
 
-  def self.validate_with_operator(input)
-    return false if input.blank?
-
-    input.strip!
-
-    # replace multiple consecutive white spaces by one single whitespace
-    input.gsub!(/\s+/,' ')
-    operator, date = input.split
-    return false if operator.nil?
-    return false unless %w(> < >= <= =).include? operator
-    validate(date)
-  end
-
   # Usage:
   #
   # validate('13-feb-2008') # => time_object
   # validate('13-February-2008') # => time_object
   # validate('13-February-2008') # => time_object
-  # validate('30-Feb-2008') #=> false
+  # validate('99-Feb-2008') #=> false
   #
   def self.validate(input)
-    return false if input.nil?
+    return false if input.blank?
 
     input.strip!
 
@@ -33,47 +20,30 @@ class AdminDataDateValidation
     return false if input.length < 9
 
     dd,mm,yyyy = input.split('-')
-    return false if dd.nil?
-    return false if mm.nil?
-    return false if yyyy.nil?
+    return false if dd.nil? || mm.nil? || yyyy.nil?
 
-    # month must be of aleast three characters
+    # month must be of atleast three characters
     return false if mm.length < 3
 
-    mm = mm.downcase
+    mm = mm.downcase[0,3]
 
-    #get only the first three characters
-    mm = mm[0,3]
-
-    months = {'jan' => 1,
-      'feb' => 2,
-      'mar' => 3,
-      'apr' => 4,
-      'may' => 5,
-      'jun' => 6,
-      'jul' => 7,
-      'aug' => 8,
-      'sep' => 9,
-      'oct' => 10,
-      'nov' => 11,
-    'dec' => 12 }
+    months = {'jan' => 1, 'feb' => 2, 'mar' => 3, 'apr' => 4, 'may' => 5, 'jun' => 6,
+      'jul' => 7, 'aug' => 8, 'sep' => 9, 'oct' => 10, 'nov' => 11, 'dec' => 12 }
 
     return false unless months.keys.include? mm
 
-    mm = months[mm]
-    mm = mm.to_i
+    mm = months[mm].to_i
     yyyy = yyyy.to_i
     dd = dd.to_i
 
     # validate date values
     begin
       Date.new(yyyy,mm,dd)
-    rescue  => e
+    rescue
       return false
     end
 
-    t = Time.now
-    t.change(:year => yyyy, :month => mm, :day => dd, :hour => 0)
+    Time.now.change(:year => yyyy, :month => mm, :day => dd, :hour => 0)
   end
 
 end
