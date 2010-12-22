019ae3ca369f0552a443ca36c2b0815f099d2d6a
Neeraj Singh <neerajdotname@gmail.com>
2010-12-22 05:13:29 -0500

    formatting of existing code

--- lib/admin_data/config.rb
+++ lib/admin_data/config.rb
@@ -1,9 +1,11 @@
+# Following code takes care of providing a nice error
+# message to users who are using old API
 module AdminData
-class Config
 
-  cattr_accessor :setting
+  class Config
+    cattr_accessor :setting
 
-  def self.set=(input = {})
+    def self.set=(input = {})
 
 msg=<<EOF
 
@@ -23,12 +25,14 @@ AdminData.config do |config|
  config.find_conditions = ...
 end
 
-Please refer to documentation at https://github.com/neerajdotname/admin_data/wiki/Customizing-admin_data-for-a-Rails-3-application for more information.
+Please refer to documentation at
+https://github.com/neerajdotname/admin_data/wiki/Customizing-admin_data-for-a-Rails-3-application
+for more information.
 
 EOF
 
-   raise msg
-  end
+      raise msg
+    end
 
-end
+  end
 end
