e9e96814489248db83b5b0c8c56df66e1d0b90c9
Neeraj Singh <neerajdotname@gmail.com>
2011-06-30 19:31:32 -0400

    users with phone numbers

--- test/rails_root/test/unit/user_phone_test.rb
+++ test/rails_root/test/unit/user_phone_test.rb
@@ -26,6 +26,21 @@ class UserPhoneTest < MiniTest::Unit::TestCase
     record['count_data'].to_i
   end
 
+  def count_of_main_klass_records_in_hm_klass(main_klass, hm_klass, hm_relationship_name)
+    foreign_key = main_klass.reflections[hm_relationship_name].instance_variable_get('@active_record').name.foreign_key
+    raise 'foreign_key is nil' unless foreign_key
+
+    sql = %Q{
+    
+      select #{main_klass.table_name}.id, count(#{hm_klass.table_name}.id)
+      from #{main_klass.table_name} join #{hm_klass.table_name} on #{main_klass.table_name}.id = #{hm_klass.table_name}.#{foreign_key}
+      group by #{main_klass.table_name}.id
+      having count (#{hm_klass.table_name}.id) > 0
+    }
+
+    main_klass.find_by_sql(sql).size
+  end
+
   def test_pie_chart_for_user_with_phones_vs_users_without_phones
     5.times { Factory(:user) }
     3.times do
@@ -39,7 +54,8 @@ class UserPhoneTest < MiniTest::Unit::TestCase
     hm_klass = PhoneNumber
     hm_relationship = :phone_numbers
 
-    assert_equal 5, count_of_main_klass_records_not_in_hm_klass(User, PhoneNumber, :phone_numbers)
+    #assert_equal 5, count_of_main_klass_records_not_in_hm_klass(User, PhoneNumber, :phone_numbers)
+    assert_equal 3, count_of_main_klass_records_in_hm_klass(User, PhoneNumber, :phone_numbers)
   end
 end
 
