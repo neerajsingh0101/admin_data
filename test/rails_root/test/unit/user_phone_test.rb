d1f266e47b5b86aebb907ba5454b6f10b6f1d93b
Neeraj Singh <neerajdotname@gmail.com>
2011-06-30 19:04:02 -0400

    making test pass

--- test/rails_root/test/unit/user_phone_test.rb
+++ test/rails_root/test/unit/user_phone_test.rb
@@ -8,6 +8,23 @@ class UserPhoneTest < MiniTest::Unit::TestCase
     PhoneNumber.delete_all
   end
 
+  def count_of_main_klass_records_not_in_hm_klass(main_klass, hm_klass, hm_relationship_name)
+    foreign_key = main_klass.reflections[hm_relationship_name].instance_variable_get('@active_record').name.foreign_key
+
+    sql  = %Q{
+      
+      select count(*) 
+      from #{main_klass.table_name}
+      where users.id NOT IN (
+        select #{hm_klass.table_name}.#{foreign_key}
+        from #{hm_klass.table_name}
+      )
+    
+    }
+    record = main_klass.find_by_sql(sql).first
+    record['count(*)']
+  end
+
   def test_pie_chart_for_user_with_phones_vs_users_without_phones
     5.times { Factory(:user) }
     3.times do
@@ -20,28 +37,8 @@ class UserPhoneTest < MiniTest::Unit::TestCase
     main_klass = User
     hm_klass = PhoneNumber
     hm_relationship = :phone_numbers
-    require 'ruby-debug'; debugger
-    foreign_key = main_klass.reflections[hm_relationship].instance_variable_get('@active_record').name.foreign_key
 
-    sql  = %Q{
-      
-      select count(*) 
-      from #{main_klass.table_name}
-      where users.id NOT IN (
-        select #{hm_klass.table_name}.#{foreign_key}
-        from #{hm_klass.table_name}
-      )
-    
-    }
-
-    u = User.find_by_sql(sql).first
-    puts u
-
-    u2 = u['count(*)']
-    puts u2
-
-
-    assert_equal 5, u2
+    assert_equal 5, count_of_main_klass_records_not_in_hm_klass(User, PhoneNumber, :phone_numbers)
   end
 end
 
