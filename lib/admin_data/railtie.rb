ef7a63766c9a9671f496286e2c0eb8c7a22b4327
Neeraj Singh <neerajdotname@gmail.com>
2012-05-03 14:31:04 -0400

    wip

--- lib/admin_data/railtie.rb
+++ lib/admin_data/railtie.rb
@@ -2,7 +2,15 @@ module AdminData
   class Engine < Rails::Engine
 
     initializer "admin_data precompile hook" do |app|
-      app.config.assets.precompile += ['admin_data.js', 'admin_data.css']
+      app.config.assets.precompile += [
+        'advance_search/*',
+        'analytics/*',
+        'misc/*',
+        'vendor/*',
+        'vendor/jquery-ui-1.7.2.custom.css',
+        'admin_data.js',
+        'admin_data.css'
+      ]
     end
 
     rake_tasks do
