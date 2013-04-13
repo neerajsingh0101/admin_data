f7909e89a6572fd50fb63c650d0ee58f5d919d6f
Everard Brown <everard.brown@htcl.eu>
2013-04-13 17:17:23 +0200

    handle namespaced models
    - updates for processing namespaced models
    - remove ambiguity in named routes in CRUD operations
    - a few tweaks to maintain ruby 1.8.7 compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,30 @@
 AdminData::Engine.routes.draw do
 
   controller "crud" do
-    match '/klass/(:klass)',        to: :index,            :as => :index,   :via => :get
-    match '/klass/(:klass)',        to: :create,           :as => :index,   :via => :post
-    match '/klass/:klass/new',      to: :new,              :as => :new,     :via => :get
-    match '/klass/:klass/:id/del',  to: :del,              :as => :del,     :via => :delete
-    match '/klass/:klass/:id/edit', to: :edit,             :as => :edit,    :via => :get
-    match '/klass/:klass/:id',      to: :show,             :via => :get
-    match '/klass/:klass/:id',      to: :update,           :via => :put
-    match '/klass/:klass/:id',      to: :destroy,          :via => :delete
+    match '/klass/*klass/new',      :to => :new,           :via => :get,    :as => :new
+    match '/klass/*klass/:id/del',  :to => :del,           :via => :delete, :as => :delete
+    match '/klass/*klass/:id/edit', :to => :edit,          :via => :get,    :as => :edit
+    match '/klass/*klass/:id',      :to => :show,          :via => :get,    :as => :show
+    match '/klass/*klass/:id',      :to => :update,        :via => :put,    :as => :update
+    match '/klass/*klass/:id',      :to => :destroy,       :via => :delete, :as => :destroy
+    match '/klass/(*klass)',        :to => :index,         :via => :get,    :as => :index
+    match '/klass/(*klass)',        :to => :create,        :via => :post,   :as => :create
   end
 
   controller "migration" do
-    match '/migration', to: :index,  as: :migration_information
-    match '/jstest',    to: :jstest, as: :jstest
+    match '/migration', :to => :index,  :as => :migration_information
+    match '/jstest',    :to => :jstest, :as => :jstest
   end
 
-  match '/table_structure/:klass' => "table_structure#index",  :as => :table_structure
+  match '/table_structure/*klass' => "table_structure#index",  :as => :table_structure
 
-  match '/quick_search/:klass'  => "search#quick_search",    :as => :search
+  match '/quick_search/*klass'    => "search#quick_search",    :as => :search
   match '/quick_search'           => "search#quick_search"
 
-  match '/advance_search/:klass'  => "search#advance_search",  :as => :advance_search
+  match '/advance_search/*klass'  => "search#advance_search",  :as => :advance_search
   match '/advance_search'         => "search#advance_search"
 
-  match '/feed/:klasss'           => "feed#index", :defaults => { :format =>'rss' }, :as => :feed
+  match '/feed/*klasss'           => "feed#index", :defaults => { :format =>'rss' }, :as => :feed
 
   match '/public/*file'           => "public#serve", :as => :public
 
